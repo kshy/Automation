88208cf1ce96668417a5f0f84880137c5fcc4b4d
Tatyana Volovik <tvolovik@connecture.com>
2013-09-28 01:20:09 +0300

    Updated shopping tests

--- shpselenium/smoke_runtest_shopping.bat
+++ shpselenium/smoke_runtest_shopping.bat
@@ -8,8 +8,9 @@ Set currenttime=%Time::=-%
 Set currenttime=%currenttime:.=-%
 
 echo on
-python -m robot.run -A local_args.txt --test "Test Smoke test - Producer sends proposal and completes shopping - Primary" --loglevel TRACE --outputdir "Reports\shopping" robot-tests\shopping
+python -m robot.run -A chrome_args.txt --include shopping --exclude draft --loglevel TRACE --outputdir "Reports\shopping\consumer" robot-tests\shopping
 ::python -m robot.run -A local_args.txt --test "Consumer completes shopping - Child Only" --loglevel TRACE --outputdir "Reports\shopping" robot-tests\shopping
+
 
 ::
 ::python -m robot.run -A local_args.txt src/test/resources/
