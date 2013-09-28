e0af9656fd344b8c06862821075a1eb846d72d6b
Tatyana Volovik <tvolovik@connecture.com>
2013-09-28 21:04:45 +0300

    Updated shopping tests

--- shpselenium/local_runtest.bat
+++ shpselenium/local_runtest.bat
@@ -15,7 +15,7 @@ echo on
 ::python -m robot.run -A local_args.txt --test "Test Smoke test - Producer completes shopping - Primary" --loglevel TRACE --outputdir "Reports\shopping\broker" robot-tests\shopping
 ::python -m robot.run -A local_args.txt --test "Test Smoke test Enrollment via Quote - Broker" --loglevel TRACE --outputdir "Reports" robot-tests\sg\sgenrollment
 ::python -m robot.run -A local_args.txt --test "Test Smoke test Quote from case - Broker" --loglevel TRACE --outputdir "Reports\SG\quote1" robot-tests\sg\sgquote
-python -m robot.run -A local_args.txt --include smokeANDsgANDenrollment --exclude draft --loglevel TRACE --outputdir "Reports\stg\qt" robot-tests
+python -m robot.run -A local_args.txt --include smokeANDsgANDquote --exclude draft --loglevel TRACE --outputdir "Reports\qa\qt" robot-tests
 ::python -m robot.run -A local_args.txt --include smokeANDshopping --exclude draft --loglevel TRACE --outputdir "Reports" robot-tests
 
 ::python -m robot.run -A local_args.txt src/test/resources/
--- shpselenium/local_runtest_sg.bat
+++ shpselenium/local_runtest_sg.bat
@@ -8,12 +8,11 @@ Set currenttime=%Time::=-%
 Set currenttime=%currenttime:.=-%
 
 echo on
-::python -m robot.run -A local_args.txt --test "Test Smoke test SG Quote-Enroll - Broker" --loglevel TRACE --outputdir "Reports\SG" robot-tests/sg
-::python -m robot.run -A ff_args.txt --include smokeANDsgANDquote --exclude draft --loglevel TRACE --outputdir "Reports\uat\qt" robot-tests
+
 python -m robot.run -A local_args.txt --test "Test Smoke test SG Quote-Enroll - Broker" --loglevel TRACE --outputdir "Reports\uat\SG\enr" robot-tests\sg\sgquotenroll
 
-::python -m robot.run -A local_args.txt --test "Test Smoke test Quote from case - Broker" --loglevel TRACE --outputdir "Reports" robot-tests/sg
-
+::python -m robot.run -A local_args.txt --test "Test Smoke test Quote from case - Broker" --loglevel TRACE --outputdir "Reports" robot-tests\sg
+::python -m robot.run -A local_args.txt --test "Test Smoke test Enrollment from case - Broker" --loglevel TRACE --outputdir "Reports" robot-tests\sg
 
 ::Execution run
 ::python -m robot.run -A local_args.txt --outputdir "Reports" src/test/resources/robot-tests/sguserstories/sg_us_028919.txt
